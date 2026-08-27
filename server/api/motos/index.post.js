2102cba90753c5232c90826980f3929c3f57279b
Micadri <micheroux.adrien.atc@gmail.com>
2026-08-27 14:40:07 +0200

    add post

---
+++ server/api/motos/index.post.js
@@ -0,0 +1,22 @@
+export default defineEventHandler(async (e) => {
+  const body = await readBody(e) 
+
+  try {
+    const [result] = await db.query(
+      'INSERT INTO motos (marque, modele, couleur) VALUES (?, ?, ?)',
+      [body.marque, body.modele, body.couleur]
+    )
+
+    return {
+      id: result.insertID,
+      modele: body.modele,
+      marque: body.marque,
+      couleur: body.couleur
+    }
+  } catch(error) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: `Erreur MySQL: ${error.message}`
+    })
+  }
+})
